b9e2c6570417e071eb3bf99c002475b1b8a98d32
Informatica para Internet <devnet@sistemafieto.rede>
2024-05-16 11:20:16 -0300

    soma de idade

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const app = express()
-
-app.use((req,res,next)=>{
-    res.send("minha api")
+const usuario=[
+    {nome:'carlos', idade:20},
+    {nome:'pedro', idade:30,peso:100},
+    {nome:'jr', idade:30,peso:100, cep:'58221-00'}
+]
+// app.use((req,res,next)=>{
+//     res.send(usuario)
+// })
+// app.use("/nome",(req,res,next)=>{
+//     res.send(usuario[1].nome)
+// })
+app.use("/soma",(req,res,next)=>{
+    let total=0
+    for(i=0; i<usuario.length; i++){
+        total=total+parseInt(usuario[i].idade)
+    }
+    res.send('Soma total:'+total)
 })
+module.exports = app
